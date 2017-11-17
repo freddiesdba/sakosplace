d8c995c103fe27670e8717ae21fb6f3826a5433f
freddiesdba <fredsako8@gmail.com>
2017-11-17 09:46:00 +0300

    sakosplace website

---
+++ app/controllers/agency_controller.rb
@@ -0,0 +1,19 @@
+class AgencyController < ApplicationController
+  def index
+  end
+
+  def services
+  end
+
+  def portforlio
+  end
+
+  def about
+  end
+
+  def team
+  end
+
+  def contact
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  get 'agency/index'
+
+  get 'agency/services'
+
+  get 'agency/portforlio'
+
+  get 'agency/about'
+
+  get 'agency/team'
+
+  get 'agency/contact'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
